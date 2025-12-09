71b002ce6764e0c09a0131999015cf2e5636b02a
winterzard17 <jawonsss7@gmail.com>
2025-12-09 16:53:52 +0900

    Create ForLoopExample.java
    Java

---
+++ ForLoopExample.java
@@ -0,0 +1,8 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // Loop structure demonstration
+        for(int i=1; i<=5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
