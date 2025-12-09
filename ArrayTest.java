4a4b90f57df91953a88b0e6253ac7f37bfd9df44
winterzard17 <jawonsss7@gmail.com>
2025-12-09 16:54:14 +0900

    Create ArrayTest.java
    Java

---
+++ ArrayTest.java
@@ -0,0 +1,7 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        // Array manipulation
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("First element: " + numbers[0]);
+    }
+}
