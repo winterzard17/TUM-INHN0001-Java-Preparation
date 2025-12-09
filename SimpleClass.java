2ce30bc94a3d5df2e037d6fcce11115019becc06
winterzard17 <jawonsss7@gmail.com>
2025-12-09 17:02:13 +0900

    SimpleClass.java

--- SimpleClass.java
+++ SimpleClass.java
@@ -1,11 +1,14 @@
 public class SimpleClass {
     String name;
+    
     // Constructor
     public SimpleClass(String name) {
         this.name = name;
     }
+    
     public static void main(String[] args) {
-        SimpleClass obj = new SimpleClass("Student");
+        // Creating object with my name
+        SimpleClass obj = new SimpleClass("Jaewon Kim"); 
         System.out.println("Object created for: " + obj.name);
     }
 }
